f00923ce1924242aedf2334fcb838f326f37f959
BT_1N3_08 <sample@bths.edu>
2023-09-28 11:06:53 -0400

    points

--- src/RectangleRunner.java
+++ src/RectangleRunner.java
@@ -12,5 +12,13 @@ public class RectangleRunner {
 
         int total = plot1.calculateArea() + plot2.calculateArea() + plot3.calculateArea();
         System.out.println("These three plots requires " + total + " square feet of seed");
+
+        int len = 65;
+        int wid = len + 10;
+        Rectangle rect1 = new Rectangle(len, wid);
+        Rectangle rect2 = new Rectangle(10.0, 30.0);
+        Rectangle rect3 = new Rectangle(0);
+
+
     }
 }
---
+++ src/point.java
@@ -0,0 +1,20 @@
+public class point {
+    //instance variables
+    private int x;
+    private int y;
+
+    //constructors
+    public Point(int xcoord, int ycoord){
+        x = xcoord;
+        y = ycoord;
+    }
+    public Point(int coord){
+        x = coord;
+        y = coord;
+    }
+    public Point(){
+        x = 0;
+        y = 0;
+    }
+
+}
